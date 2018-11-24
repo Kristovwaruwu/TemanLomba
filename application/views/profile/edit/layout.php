a90d455aeac77dd612564667f9f75438a28bbb73
AudhikagaZ <audhikaga@gmail.com>
2018-11-24 22:34:58 +0700

    Profile Edit

--- application/views/profile/edit/layout.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$this->load->view('profile/header');
-$this->load->view('profile/menu');
-$this->load->view('profile/'.$page,$data);
-$this->load->view('profile/footer');
