f9f4313f263bcfc5bfdb3be5c4831d9cbec8576a
AudhikagaZ <audhikaga@gmail.com>
2018-11-24 19:15:32 +0700

    Profile

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -3,10 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Profile extends CI_Controller {
 
+    function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->load->model('DB');
+    }
+    
 	function index(){
 		$params['page'] = 'home/index';
 		$params['data']['tes']='aaaa';
 		$this->load->view('profile/layout',$params);
 	}
-
 }
---
+++ application/views/profile/edit/index.php
@@ -0,0 +1,9 @@
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+          <div class="row">
+          <?php if(!empty($lomba)){ 
+            foreach ($lomba as $data) {?>
+          <p>$data</p>
+            <?php }}?>
+
+      
+</main>
