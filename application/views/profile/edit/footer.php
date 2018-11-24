0d01ce726e1a73c1a35592cec225d4fb8b20c24d
teguh <cs.teguhsubiyantoro@gmail.com>
2018-11-24 23:00:03 +0700

    aaa

--- application/views/profile/edit/footer.php
+++
@@ -1,8 +0,0 @@
-<script src="<?= base_url() ?>assets/js/vue.js" type="text/javascript"></script>
-<script src="<?= base_url() ?>assets/js/axios.min.js" type="text/javascript"></script>
-<script src="<?= base_url() ?>assets/js/jquery-3.3.1.slim.min.js" type="text/javascript"></script>
-<script src="<?= base_url() ?>assets/js/bootstrap.min.js" type="text/javascript"></script>
-
-<script src="<?= base_url() ?>assets/js/app.js" type="text/javascript"></script>
-</body>
-</html>
