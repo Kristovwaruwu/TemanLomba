135fb789d95eef97dde88933b56667cdfb180ae9
Kristov <kristoveykw@gmail.com>
2018-11-24 18:04:38 +0700

    login

--- application/views/front/menu.php
+++ application/views/front/menu.php
@@ -4,7 +4,7 @@
           <nav class="nav nav-masthead nav-custom">
             <a class="nav-link active" href="#">Home</a>
             <a class="nav-link" href="#">Dashboard</a>
-            <button href="#" class="btn btn-danger " role="login" data-toggle="modal" data-target="#loginModal">Login</button>
+            <button class="btn btn-danger" role="login" data-toggle="modal" data-target="#loginModal">Login</button>
           </nav>
          
         </div>
--- application/views/loginmodal.php
+++ application/views/loginmodal.php
@@ -2,7 +2,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
+        <h5 class="modal-title">Login</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -10,24 +10,18 @@
       <div class="modal-body" >
         <form action="<?php echo base_url()?>user/login" method="post">
 			<div class="form-group" >
-			  <label for="inputEmail">Email address</label>
 			  <input name="Username" class="form-control" id="inputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
 			  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
 			</div>
 			<div class="form-group">
-			  <label for="inputPassword">Password</label>
 			  <input name="Password" type="password" class="form-control" id="inputPassword1" placeholder="Password">
 			</div>
-			<div class="form-group form-check">
-			  <input type="checkbox" class="form-check-input" id="exampleCheck1">
-			  <label class="form-check-label" for="exampleCheck1">Check me out</label>
-			</div>
-			<button type="submit" class="btn btn-primary">Submit</button>
-		  </form>
+			<div class="custom-control custom-checkbox mb-3">
+        <input type="checkbox" class="custom-control-input" id="customCheck1">
+        <label class="custom-control-label" for="customCheck1">Remember password</label>
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+			<button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
+		  </form>
       </div>
     </div>
   </div>
