6840ce961535ea828562dea5ee8580a6e78bc384
teguh <cs.teguhsubiyantoro@gmail.com>
2018-11-24 23:05:55 +0700

    aaa

--- application/views/front/home/index.php
+++ application/views/front/home/index.php
@@ -1,5 +1,5 @@
 <main role="main" class="inner cover">
-        <h1 class="cover-heading"><img width="250" src="<?= base_url() ?>assets/img/logo.png"></h1>
+        <h1 class="cover-heading">Temanlomba</h1>
         <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
         <p class="lead">
           <a href="#" class="btn btn-lg btn-secondary btn-home">Mulai cari partnermu hari ini!!</a>
