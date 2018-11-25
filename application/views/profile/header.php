72a2d9f8764887d4959263cbaea03b7bb11da4c4
Kristov <kristoveykw@gmail.com>
2018-11-25 07:48:46 +0700

    aaa

--- application/views/profile/header.php
+++ application/views/profile/header.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Teman Lomba</a>
+<a class="navbar-brand col-sm-3 col-md-2 mr-0" href="<?php echo base_url() ?>dashboard/lomba"><img src="<?php echo base_url() ?>assets/img/logoTLata.png" height="50%" width="70%"></a>
       <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
